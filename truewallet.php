340598d0554ca61faecac9c5eac21e211b4bb9cf
Thanapiphat <58478507+Jindanet@users.noreply.github.com>
2024-06-29 19:42:03 +0700

    Create truewallet.php

---
+++ truewallet.php
@@ -0,0 +1,34 @@
+<?php
+function giftcode($hash = null, $phone = null) {
+  if (is_null($hash) || is_null($phone)) return false;
+  $ch = curl_init();
+  @$hash = explode('?v=', $hash)[1];
+  $headers  = [
+    'Content-Type: application/json',
+    'Accept: application/json'
+  ];
+  $postData = [
+    'mobile' => $phone,
+    'voucher_hash' => $hash
+  ];
+  curl_setopt($ch, CURLOPT_URL,"https://gift.truemoney.com/campaign/vouchers/$hash/redeem");
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_SSLVERSION, 7);
+  curl_setopt($ch, CURLOPT_USERAGENT, "aaaaaaaaaaa");
+  $response = curl_exec($ch);
+  echo $response;
+  return $response;
+}
+
+$hash = $_GET["link"];
+$phone = "0617584255";
+
+$gift = giftcode($hash, $phone);
+
+/** 
+ GET = https://yourdomain.com/truewallet.php?link=https://gift.truemoney.com/campaign/?v=xxxxxx
+**/
+?>
